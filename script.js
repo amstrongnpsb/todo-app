f914a83a7424eb36803b80f8288ade10da987eb2
amstrongnpsb <amstrongpsb@gmail.com>
2023-10-05 20:29:41 +0700

    change logo in new task

--- script.js
+++ script.js
@@ -23,11 +23,11 @@ const formBtn = addBtn.addEventListener("click", function () {
     const createAddForm = () => {
       const addFormCard = document.createElement("div");
       addFormCard.innerHTML = `<input type="text" placeholder="Enter New Task"
-            class="input-form w-11/12 m-2 rounded-lg p-1 focus:ring-zinc-800 focus:outline-none focus:ring-4 text-zinc-800 pl-4"><i
+            class="input-form w-10/12 m-2 rounded-lg p-1 focus:ring-zinc-800 focus:outline-none focus:ring-4 text-zinc-800 pl-4"><i
             class="addtask-button fa-solid fa-plus bg-zinc-800 rounded-full text-zinc-100 p-1 cursor-pointer"></i>`;
       addFormCard.classList.add(
         "add-form",
-        "w-10/12",
+        "w-5/12",
         "bg-slate-300",
         "mt-3",
         "rounded-xl",
